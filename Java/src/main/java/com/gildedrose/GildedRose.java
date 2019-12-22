e0ed4d9311afb32060e6c1e1972fed23c92067e7
mperezi <mperezibars@gmail.com>
2019-12-22 09:05:57 +0100

    Extract update item quality logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,31 +16,27 @@ class GildedRose {
         this.items = items;
     }
 
+    private void updateItemQuality(Item item, int delta) {
+        int newQuality = item.quality + delta;
+        if (newQuality >= MIN_QUALITY && newQuality <= MAX_QUALITY) {
+            item.quality = newQuality;
+        }
+    }
+
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals(AGED_BRIE_ITEM)
-                && !item.name.equals(BACKSTAGE_PASS_ITEM)) {
-                if (item.quality > MIN_QUALITY) {
-                    if (!item.name.equals(LEGENDARY_ITEM)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+                && !item.name.equals(BACKSTAGE_PASS_ITEM)
+                && !item.name.equals(LEGENDARY_ITEM)) {
+                updateItemQuality(item, -1);
             } else {
-                if (item.quality < MAX_QUALITY) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASS_ITEM)) {
-                        if (item.sellIn < BACKSTAGE_DEADLINE_1) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < BACKSTAGE_DEADLINE_2) {
-                            if (item.quality < MAX_QUALITY) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                updateItemQuality(item, 1);
+                if (item.name.equals(BACKSTAGE_PASS_ITEM)) {
+                    if (item.sellIn < BACKSTAGE_DEADLINE_1) {
+                        updateItemQuality(item, 1);
+                    }
+                    if (item.sellIn < BACKSTAGE_DEADLINE_2) {
+                       updateItemQuality(item, 1);
                     }
                 }
             }
@@ -50,19 +46,15 @@ class GildedRose {
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE_ITEM)) {
-                    if (!item.name.equals(BACKSTAGE_PASS_ITEM)) {
-                        if (item.quality > MIN_QUALITY) {
-                            if (!item.name.equals(LEGENDARY_ITEM)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
+                if (item.name.equals(AGED_BRIE_ITEM)) {
+                    updateItemQuality(item, 1);
                 } else {
-                    if (item.quality < MAX_QUALITY) {
-                        item.quality = item.quality + 1;
+                    if (item.name.equals(BACKSTAGE_PASS_ITEM)) {
+                        item.quality = 0;
+                    } else {
+                        if (!item.name.equals(LEGENDARY_ITEM)) {
+                            updateItemQuality(item, -1);
+                        }
                     }
                 }
             }
